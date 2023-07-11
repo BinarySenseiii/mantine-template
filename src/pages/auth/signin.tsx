15cb809faafdf285c4ea13d97bc854cba0eef519
Faisal Tariq <codewithtyson@gmail.com>
2023-07-11 16:16:33 +0400

    deps: up

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,37 +1,37 @@
-import {Button, Center} from '@mantine/core'
-import type {GetServerSidePropsContext, NextPage} from 'next'
-import {signIn} from 'next-auth/react'
-import {getServerAuthSession} from '~/server/auth'
+import { Button, Center } from '@mantine/core'
+import type { GetServerSidePropsContext, NextPage } from 'next'
+import { signIn } from 'next-auth/react'
+import { getServerAuthSession } from '~/server/auth'
 
 const SignInPage: NextPage = () => {
-  return (
-    <Center m={4}>
-      <Button
-        m={4}
-        onClick={() => signIn('google', {callbackUrl: '/'})}
-        tt="capitalize"
-      >
-        Sign In with Google
-      </Button>
-    </Center>
-  )
+	return (
+		<Center m={4}>
+			<Button
+				m={4}
+				onClick={() => signIn('google', { callbackUrl: '/' })}
+				tt="capitalize"
+			>
+				Sign In with Google
+			</Button>
+		</Center>
+	)
 }
 
 export default SignInPage
 
 export async function getServerSideProps(ctx: GetServerSidePropsContext) {
-  const session = await getServerAuthSession(ctx)
+	const session = await getServerAuthSession(ctx)
 
-  if (session) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    }
-  }
+	if (session) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			},
+		}
+	}
 
-  return {
-    props: {},
-  }
+	return {
+		props: {},
+	}
 }
