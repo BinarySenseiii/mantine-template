5b7264ae20ff5b9f2f31c0db1826d5d3059d9725
Faisal TariQ <faisaltariq183@gmail.com>
2023-09-11 20:40:28 +0400

    Update axios-client.ts

--- src/lib/axios-client.ts
+++ src/lib/axios-client.ts
@@ -1,37 +1,31 @@
-import type {AxiosInstance, AxiosRequestConfig, AxiosResponse} from 'axios'
+import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
 import axios from 'axios'
+import { env } from '~/env.mjs'
 
 const RICK_MORTY_BASEURL = 'https://rickandmortyapi.com/api'
-const NEXT_API_BASEURL = '/api'
 
-/* export const urls = {
-  test: `http://localhost:3334`,
-  development: 'https://rickandmortyapi.com/api',
-  production: NO_CODE_API_BASEURL,
-} */
+const URLS = {
+	development: RICK_MORTY_BASEURL,
+	production: RICK_MORTY_BASEURL,
+}
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data
 
 export const options = {
-	headers: {'content-type': 'application/json'},
+	headers: { 'content-type': 'application/json' },
 }
 
-const createInstance = (baseURL: string, options: {}) =>
-	axios.create({baseURL, ...options})
+const createInstance = (baseURL: string, options: AxiosRequestConfig = {}) =>
+	axios.create({ baseURL, ...options })
 
 export const request = {
-	// rickMorty: createInstance(RICK_MORTY_BASEURL, options),
-	rickMorty: createInstance(RICK_MORTY_BASEURL, {}),
-	nextAPi: createInstance(NEXT_API_BASEURL, {}),
+	rickMorty: createInstance(URLS[env.NODE_ENV], {}),
 }
 
 export const requestActions = {
-	get: <T>(
-		instance: AxiosInstance,
-		url: string,
-		config: AxiosRequestConfig = {},
-	) => instance.get<T>(url, config).then(responseBody),
+	get: <T>(instance: AxiosInstance, url: string, config: AxiosRequestConfig = {}) =>
+		instance.get<T>(url, config).then(responseBody),
 
-	post: <T>(instance: AxiosInstance, url: string, body: {}) =>
+	post: <T>(instance: AxiosInstance, url: string, body: unknown) =>
 		instance.post<T>(url, body).then(responseBody),
 }
